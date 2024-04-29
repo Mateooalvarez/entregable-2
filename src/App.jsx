f861225787a508d53e55e78d3e8bc63f37bbc718
MateoDev <165196142+Mateooalvarez@users.noreply.github.com>
2024-04-29 01:20:45 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+import WeatherCard from './assets/components/WeatherCard';
+
+function App() {
+  const [coords, setCoords] = useState();
+  const [weather, setWeather] = useState(); 
+  const [temp, setTemp] = useState({ celsius: '', fahrenheit: '' });
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
+  const [showMessage, setShowMessage] = useState(false);
+ 
+  useEffect(() => {
+    setTimeout(() => {
+      setShowMessage(true);
+    }, 3000);
+
+    const success = (pos) => {
+      setCoords({
+        lat: pos.coords.latitude,
+        lon: pos.coords.longitude
+      });
+    };
+
+    const error = () => {
+      setHasError(true);
+      setIsLoading(false);
+    };
+   
+    navigator.geolocation.getCurrentPosition(success, error);
+  }, []);
+
+  useEffect(() => {
+    if (coords) {
+      const API_KEY = '19b31a421a36259d849cdb0f494fa7c0';
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${API_KEY}`;
+   
+      axios.get(url)
+        .then(res => { 
+          setWeather(res.data);
+          const celsius = (res.data.main.temp - 273.15).toFixed(1);
+          const fahrenheit =  (celsius * 9/5 + 32).toFixed(1);
+          setTemp({ celsius, fahrenheit });
+        })
+        .catch(err => {
+          console.log(err);
+          setHasError(true);
+        })
+        .finally(() => setIsLoading(false));
+    }
+  }, [coords]);
+
+  console.log(weather);
+  
+  return (
+    <div className='app'>
+      {isLoading ? (
+        <div>
+          <h1 style={{ color: 'white' }}>loading...</h1>
+          <p style={{ color: '#ccc' }}>Por favor, activa la ubicación</p>
+        </div>
+      ) : (
+        hasError ? (
+          <h1>Para obtener el clima de tu ciudad, por favor permite la ubicación.</h1>
+        ) : (
+          <WeatherCard
+            weather={weather}
+            temp={temp}
+            setTemp={setTemp}
+            showMessage={showMessage}
+          />
+        )
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/components/WeatherCard.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+const WeatherCard = ({ weather, temp }) => {
+
+ const [isCelsius, setIsCelsius] = useState(true)
+ const [temperature, setTemperature] = useState(temp)
+
+ const changeTemperature = () => {
+    setIsCelsius(!isCelsius);
+    if (isCelsius) {
+      const fahrenheit = (temp.celsius * 9/5 + 32).toFixed(1);
+      setTemperature({ ...temperature, fahrenheit });
+    } else {
+      const celsius = ((temp.fahrenheit - 32) * 5/9).toFixed(1);
+      setTemperature({ ...temperature, celsius });
+    }
+  };
+
+    return (
+      <article>
+        <h1>Clima</h1>
+        <h2>{weather?.name}, {weather?.sys.country}</h2>
+        <section>
+            <div>
+            <img
+  src={weather && weather.weather && weather.weather[0] && `https://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`}
+  alt=""
+/>
+            </div>
+        <article>
+            <h3>{weather?.weather[0].description}</h3>
+            <ul>
+                <li><span>Viento:</span><span>{weather?.wind.speed}m/s</span></li>
+                <li><span>Nubes:</span><span>{weather?.clouds.all}%</span></li>
+                <li><span>Humedad:</span><span></span>{weather?.main.humidity}hPa</li>
+            </ul>
+        </article>
+        </section>
+        <h2>{temp && isCelsius ? `${temp.celsius}°C` : `${temp.fahrenheit}°F`} </h2>
+        <button onClick={changeTemperature}>Change to {isCelsius ? '°F' : '°C'}</button>
+      </article>
+    )
+}
+export default WeatherCard
